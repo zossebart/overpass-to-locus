c77d75359adebe69a4d79394c05d4de5f6b01495
zossebart <zossebart@googlemail.com>
2018-04-12 14:37:52 +0200

    handle parameters more generally

--- op2gpx-locus.php
+++ op2gpx-locus.php
@@ -1,40 +1,24 @@
 <?php
 
-//if(isset($_GET['data']))$data = $_GET['data']; else $data="";
-if(isset($_GET['url']))$url = $_GET['url']; else $url="";
-if(isset($_GET['query']))$query = $_GET['query']; else $query="";
-if(isset($_GET['act']))$action = $_GET['act']; else $action="";
-if(isset($_GET['naming']))$naming = $_GET['naming']; else $naming="";
-if(isset($_GET['timebase']))$timebase = $_GET['timebase']; else $timebase="";
-if(isset($_GET['shpmode']))$shpmode = $_GET['shpmode']; else $shpmode="";
-
-$redirecturl = "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME'])."/op2gpx.php";
-
 $redirecturl = "<![CDATA[http://".$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME'])."/op2gpx.php";
 
-if($query!=""){
-	error_log("got query: $query");
-	$redirecturl = $redirecturl."?query=".urlencode($query); 	//op query -> urlencode		
+$pcount = 0;
+foreach($_GET as $idx => $val){
+	if($pcount++ == 0)
+		$redirecturl .= "?";
+	else
+		$redirecturl .= "&";
+
+	if($idx != "act"){	// act is handled by this script later
+		$redirecturl .= $idx."=";
+
+		if($idx == "query")
+			$redirecturl .= urlencode($val); // query has to be re-urlencoded
+		else
+			$redirecturl .= $val; // no special meaning, simply pass the param
+	}
 }
-else if($url!=""){
-	error_log("got url: $url");
-	$redirecturl = $redirecturl."?url=".$url;				//full op url with urlencoded query
-}
-//error_log($url);
-//error_log("action: '$action'");
-
-if($naming!="")
-	$redirecturl = $redirecturl."&naming=".$naming;
-
-if($timebase!="")
-	$redirecturl = $redirecturl."&timebase=".$timebase;	
-
-if($shpmode!="")
-	$redirecturl = $redirecturl."&shpmode=".$shpmode;	
-
 $redirecturl .= "]]>";
-
-//error_log($redirecturl);
 
 print("<?xml version=\"1.0\" encoding=\"utf-8\"?>
 	<locusActions>
@@ -44,7 +28,7 @@ print("<?xml version=\"1.0\" encoding=\"utf-8\"?>
 	        </source>
 	        <dest><![CDATA[/mapItems/op2gpx.gpx]]></dest>");
 
-if($action == "import")
+if(isset($_GET['act']) && $_GET['act'] == "import")
 	print("<after>importData</after>");	
 else
 	print("<after>displayData</after>");
